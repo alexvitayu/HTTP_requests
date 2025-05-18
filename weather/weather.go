1b98b5b02fb8646ffc8b42f75ab586ab797dd636
alexvitayu <alexvitayu1212@gmail.com>
2025-05-18 10:35:02 +0300

    feat: weather_negative test

--- weather/weather.go
+++ weather/weather.go
@@ -2,30 +2,34 @@ package weather
 
 import (
 	"HTTP_requests/weather/geo"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 )
 
-func GetWeather(geo geo.GeoData, format int) string {
+var ErrWrongFormat = errors.New("Wrong_format")
+
+func GetWeather(geo geo.GeoData, format int) (string, error) {
+	if format < 1 || format > 4 {
+		return "", ErrWrongFormat
+	}
 	baseUrl, err := url.Parse("https://wttr.in/" + geo.City)
 	if err != nil {
-		fmt.Println(err.Error())
-		return ""
+		return "", errors.New("ErrBaseUrl_Weather")
 	}
 	params := url.Values{}
 	params.Add("format", fmt.Sprint(format))
 	baseUrl.RawQuery = params.Encode()
 	resp, err := http.Get(baseUrl.String())
 	if err != nil {
-		fmt.Println(err.Error())
-		return ""
+		return "", errors.New("ErrResponse_Weather")
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err.Error())
+		return "", errors.New("ErrRespBody_Weather")
 	}
-	return string(body)
+	return string(body), nil
 }
--- weather/weather_test.go
+++ weather/weather_test.go
@@ -3,6 +3,7 @@ package weather_test
 import (
 	"HTTP_requests/weather/geo"
 	"HTTP_requests/weather/weather"
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -15,10 +16,42 @@ func TestGetWeather(t *testing.T) {
 	}
 	format := 3
 	//Act
-	got := weather.GetWeather(geo, format)
+	got, _ := weather.GetWeather(geo, format)
 	//Assert
 	if !strings.Contains(got, expected) {
 		t.Errorf("ожидали %v, получили %v", expected, got)
+	}
+
+}
+
+func TestGetWeatherWrongFormat(t *testing.T) {
+	//Arrange
+	expected := "Brest"
+	geo := geo.GeoData{
+		City: expected,
+	}
+	format := 5
+	//Act
+	got, _ := weather.GetWeather(geo, format)
+	//Assert
+	if !strings.Contains(got, fmt.Sprint(format)) {
+		t.Errorf("ожидали %v, получили %v", expected, got)
+	}
+}
+
+func TestGetWeatherWrongFormatTheSecond(t *testing.T) {
+	//Arrange
+	city := "Brest"
+	geo := geo.GeoData{
+		City: city,
+	}
+	format := -1
+	//Act
+	_, err := weather.GetWeather(geo, format)
+	//Assert
+	if err != weather.ErrWrongFormat {
+		t.Errorf("ожидали %v, получили %v", weather.ErrWrongFormat, err)
+
 	}
 
 }
